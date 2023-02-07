734bca53d93d024b00db60c14078269184f99bf2
Francois Ward <fward@hey.com>
2023-02-06 23:24:33 -0500

    Also symlink the fish config

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 # solve issue where github codespaces doesn't properly handle .config directory
 ln -s ./.config/* $HOME/.config/
+
+# fish directory will already exist
+ln -s -f  ./config/fish/config.fish $HOME/.config/fish/config.fish

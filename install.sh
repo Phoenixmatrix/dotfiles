7e00a032aca6fc16f10a92638593927bb5af8fc6
Francois Ward <fward@phoenixmatrix.com>
2023-02-07 11:17:56 -0500

    Temp fix

--- install.sh
+++ install.sh
@@ -9,7 +9,5 @@ do
 done
 
 # solve issue where github codespaces doesn't properly handle .config directory
-ln -s $PWD/.config/* $HOME/.config/
-
-# fish directory will already exist
 ln -s -f $PWD/.config/fish/config.fish $HOME/.config/fish/config.fish
+ln -s $PWD/.config/starship.toml $HOME/.config/starship.toml

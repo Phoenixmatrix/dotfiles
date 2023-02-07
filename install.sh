91423ea043f0cb32d21680b47475a9e03a8a11f2
Francois Ward <fward@hey.com>
2023-02-06 23:35:12 -0500

    Expand path

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # solve issue where github codespaces doesn't properly handle .config directory
-ln -s ./.config/* $HOME/.config/
+ln -s $PWD/.config/* $HOME/.config/
 
 # fish directory will already exist
-ln -s -f  ./config/fish/config.fish $HOME/.config/fish/config.fish
+ln -s -f $PWD/config/fish/config.fish $HOME/.config/fish/config.fish

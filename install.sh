6110a01eb98c0deb9fc14cda17cec280aef84bd8
Francois Ward <fward@hey.com>
2023-02-07 00:18:23 -0500

    symlink the root files

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+# start by symlinking the top level stuff
+for f in ./.*
+do
+  if [[ -f $file ]]; then
+    ln -s $PWD/$file $HOME/$file;
+  fi
+done
+
 # solve issue where github codespaces doesn't properly handle .config directory
 ln -s $PWD/.config/* $HOME/.config/
 

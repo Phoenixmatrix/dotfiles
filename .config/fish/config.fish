cd861f080b384b1d70be52a005e47bde493134db
Francois Ward <fward@phoenixmatrix.com>
2023-03-10 14:36:45 -0500

    Add keychain

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
     zoxide init fish | source
-    eval (ssh-agent -c) > /dev/null
+    keychain -q --ignore-missing ~/.ssh/id_rsa
+    source $HOME/.keychain/$HOSTNAME-fish
 
     # should have been set during docker image build. Slow so commented out by default
     # nvm use default > /dev/null
